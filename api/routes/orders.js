44e85d1167d219dad9e04428778f5ead708d429c
pratapvarun <43708682+pratapvarun@users.noreply.github.com>
2020-05-12 19:05:18 +0530

    Handling Errors

---
+++ api/routes/orders.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+// Handle incoming GET requests to /orders
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Orders were fetched'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Orders were created'
+    });
+});
+
+router.get('/:orderId', (req, res, next) => {
+    res.status(201).json({
+    	message: 'Order details',
+    	orderId: req.params.orderId
+    });
+})
+
+router.delete('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order delete',
+        orderId: req.params.orderId
+    });
+});
+
+
+module.exports = router;
